47c6a0b65d2dc6f269bb5d41cbd0d3d862f029bd
Goongam <dlwjddn341@naver.com>
2023-02-13 02:08:35 +0900

    Feat: memberList Drawer

--- src/components/RoomMembers.tsx
+++ src/components/RoomMembers.tsx
@@ -1,9 +1,27 @@
 import { useSocket } from '@/hooks/useSocket';
 import {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const Members = styled.div<{isOpen:boolean}>`
+    height: ${props => props.isOpen ? '100px' : '30px'};
+    border: 1px solid black;
+    transition-duration: 0.1s;
+    transition-property: height;
+    /* overflow-y: scroll; */
+    display: flex;
+`;
+
+const MemberList = styled.div`
+    overflow-y: scroll;
+    height: 100px;
+    width: 500px;
+`
 
 export default function RoomMembers(){
 
     const [members, setMembers] = useState([]);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     const {socket} = useSocket();
 
     useEffect(()=>{
@@ -14,10 +32,21 @@ export default function RoomMembers(){
     },[socket]);
 
     return (
-        <div>
+
+        <Members isOpen={isOpen}>
+            <button onClick={()=>{setIsOpen(!isOpen)}}>
+                {isOpen ? '▲' : '▼'}
+            </button>
             {
-                members.map((member)=>(<div key={member}>{member}</div>))
+                isOpen ?    
+                    <MemberList>
+                        {members.map((member)=>(<div key={member}>{member}</div>)) }
+                    </MemberList>
+                :
+                    members.length + '명'
             }
-    </div>
+        </Members>
+
+        
     );
 }
